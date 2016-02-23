5950b9e8aea25f849dc32e086bc331624f61c1b3
dhiraj-patel <dhirajsuperhero@gmail.com>
2016-02-22 21:19:44 -0500

    chnaged knightstour to an open tour 7x7 works perfectly now

--- 03KnightsTour/KnightBoard.java
+++ 03KnightsTour/KnightBoard.java
@@ -1,6 +1,7 @@
 public class KnightBoard{
     private int[][] board;
     private int totalSize;
+
     public KnightBoard(int n){
 	board = new int[n+4][n+4];
 	totalSize = (n+4)*(n+4);
@@ -27,75 +28,38 @@ public class KnightBoard{
 	}
 
     }
-    public boolean addKnight(int r,int c, int num){
-	if(board[r][c] == 0){
-	    board[r][c] = num;
-	    return true;
-	}
-	else{
+    
+    public boolean solveH(int r, int c, int step){
+	if(r<2||r>= board.length-2||c<2||c>=board[0].length-2){
 	    return false;
 	}
-
-    }
-    public boolean removeKnight(int r,int c,int num){
-	if(board[r][c]>0){
-	    board[r][c] = 0;
+	if(step==board.length*board[0].length){
 	    return true;
 	}
-	else{
+	if(board[r][c]!=0){
 	    return false;
 	}
-    }
+	for(int x=0;x<board.length;x++){
+	    for(int y=0;y<board[0].length;y++){
+		if(board[x][y]>=step){
+		    board[x][y]=0;
+		}
+	    }
+	}
+	board[r][c]=step;
+	return solveH(r-1,c+2,step+1)|| solveH(r+2,c-1,step+1)||
+	    solveH(r-2,c+1,step+1)||
+	    solveH(r+1,c+2,step+1)||
+	    solveH(r+2,c+1,step+1)||
+	    solveH(r-1,c-2,step+1)||
+	    solveH(r-2,c-1,step+1)||
+	    solveH(r+1,c-2,step+1);
+	    }
+    
     public boolean solve(){
-	for(int r = 2;r<board.length-2;r++){
-	    for(int c = 2; c< board[0].length - 2;c++){
-		if(solveH(r,c,1)){
-		    return true;
-		}
-	    }
-	}
-	
-	return false;
-    }
-    
-
-
-    public boolean solveH(int row,int col, int step){
-	boolean works = false;
-	// checks the borders
-	if(row<2||row>= board.length-2||col<2||col>=board[0].length-2){
-	    return false;
-	}
-	// checks the step border move
-	if(step == ((board.length-4) * (board[0].length -4)+1)){
-	    return true;
-	}
-	// past the basecases
-	else{
-	    if(addKnight(row,col,step)){
-		works = false ||
-		    solveH(row + 1,col+2,step+1) ||
-		    solveH(row + 1,col-2,step+1) ||
-		    solveH(row + 2,col+1,step+1) ||
-		    solveH(row + 2,col-1,step+1) ||
-		    solveH(row - 2,col+1,step+1) ||
-		    solveH(row - 2,col-1,step+1) ||
-		    solveH(row - 1,col+2,step+1) ||
-		    solveH(row - 1,col+2,step+1);
-		if(works){
-		    return true;
-		}else{
-		    removeKnight(row,col,step);
-		}
-	    }else{
-		return false;
-	    }
-	}
-	return false;
+	return solveH(2,2,1);
     }
 
-	
-    
     public void printSolution(){
     String ans = "";	
     for(int r=2;r <board.length-2;r++){
@@ -111,10 +75,13 @@ public class KnightBoard{
     }
     System.out.println(ans);
     }
+
+
     public static void main(String[]args){
-	KnightBoard kb = new KnightBoard(5,6);
+	KnightBoard kb = new KnightBoard(7);
 	kb.solve();
 	kb.printSolution();
+
     }
 
 

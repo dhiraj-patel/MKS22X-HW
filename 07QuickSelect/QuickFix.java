fb00f81200f5d8befb95531fd146e44ee0879edd
dhiraj-patel <dhirajsuperhero@gmail.com>
2016-03-14 01:38:17 -0400

    finished quick

--- 07QuickSelect/QuickFix.java
+++ 07QuickSelect/QuickFix.java
@@ -1,6 +1,22 @@
-import java.util.*;
 public class QuickFix{
-    //OLD QUICK CODE
+    public static String name(){
+	return "7,Patel,Dhiraj";
+    }
+    public static int quickselectOld(int[]data, int k) {
+	return quickselectOld(data, 0, data.length -1, k);
+    }
+ 
+    private static int quickselectOld(int[]data, int first, int last, int k) {
+	int i = partition(data,first,last);
+	if(i==k){
+	    return data[k];
+	}else if(i>k){
+	    return quickselectOld(data,first,i-1,k);
+	}else{
+	    return quickselectOld(data,i+1,last,k);
+	}
+    }
+ 
     private static int partitionOld(int[] data, int first, int last) {
 	int pivot = first + new Random().nextInt(last - first + 1);
 	swap(data, last, pivot);
@@ -13,8 +29,11 @@ public class QuickFix{
 	swap(data, first, last);
 	return first;
     }
-
-    
+    private static void swap(int[]data, int i, int j) {
+	int temp = data[i];
+	data[i] = data[j];
+	data[j] = temp;
+    }
     public static void quickSortOld(int[]data){
 	quickSortOld(data, 0, data.length -1);
     }
@@ -33,36 +52,6 @@ public class QuickFix{
 	ans += data[data.length-1]+" }";
 	System.out.println(ans);
     }
-    private static void swap(int[]data, int i, int j) {
-	int temp = data[i];
-	data[i] = data[j];
-	data[j] = temp;
-    }
 
 
-    
-    public static String name(){
-	return "7,Patel,Dhiraj";
-    }
-    public static int quickselectOld(int[]data, int k) {
-	return quickselect(data, 0, data.length -1, k);
-    }
- 
-    private static int quickselectOld(int[]data, int first, int last, int k) {
-	int i = partitionOld(data,first,last);
-	if(i==k){
-	    return data[k];
-	}else if(i>k){
-	    return quickselectOld(data,first,i-1,k);
-	}else{
-	    return quickselectOld(data,i+1,last,k);
-	}
-    }
- 
-    // END OLD QUICK CODE
-    
-    
-    
-    
-    
 }

a01d220770cf9b06a503d1764ec12b869666e9e5
dhiraj-patel <dhirajsuperhero@gmail.com>
2016-04-04 19:55:19 -0400

    actually made a diff consturctor

--- 10DoubleEndedQueue/MyDeque.java
+++ 10DoubleEndedQueue/MyDeque.java
@@ -1,37 +1,12 @@
 import java.util.*;
 public class MyDeque<T>{
+    private T[]deque;
     private int start,end,size;
-    private T[] array;
-    public MyDeque(){
-	array=(T[]) new Object[32];
+    @SuppressWarnings("unchecked");
+    public MyDeque(int _size){
+	deque=(T[])(new Object[_size]);
 	start=0;
-	end=0;
+	end=_size-1;
 	size=0;
     }
-
-    public boolean isEmpty(){
-	return size==0;
-    }
-    public String toString() {
-	String toReturn = "";
-	toReturn += "Head: ";
-	toReturn += ("" + head);
-	toReturn += "\nTail: " + ("" + tail);
-	toReturn += "\n[";
-	for (Object i : data) {
-	    toReturn += i + ", ";
-	}
-	toReturn += "]";
-	return toReturn;
-    }
     
-	    
-	
-       
-	  
-	   
-    
-
-
-
-}

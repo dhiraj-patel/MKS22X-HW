806fdfd0ab4eab90723a43eb56791d39e6d6f0be
dhiraj-patel <dhirajsuperhero@gmail.com>
2016-04-04 20:42:02 -0400

    added grow method

--- 10DoubleEndedQueue/MyDeque.java
+++ 10DoubleEndedQueue/MyDeque.java
@@ -9,4 +9,21 @@ public class MyDeque<T>{
 	end=_size-1;
 	size=0;
     }
-    
+    public MyDeque(){
+	this(10);
+    }
+    public int size(){
+	return size;
+    }
+    @SuppressWarnings("unchecked")
+    public void grow(){
+	if(size==deque.length){
+	    T[]temp=(T[])(new Object[size*2]);
+	    for(int i = 0; i < size; i++){
+		temp[i] = deque[(head+i)%size];
+	    }
+	    deque=temp;
+	    start=0;
+	    end=size-1;
+	}
+    }

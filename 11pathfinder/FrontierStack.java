62aec7290af29ba0febd3b8624f2967afea1133f
dhiraj-patel <dhirajsuperhero@gmail.com>
2016-04-11 12:48:52 -0400

    forgot to push the fstack and fqueue and finterface

---
+++ 11pathfinder/FrontierStack.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class FrontierStack<T> implements Frontier<T>{
+    private Stack<T> frontier;
+    public void add(T element){
+	frontier.push(element);
+    }
+    public T next(){
+	return frontier.pop();
+    }
+    public boolean hasNext(){
+	return !frontier.empty();
+    }
+
+}

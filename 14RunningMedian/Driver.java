e480bf6d02cd051eb0dc40c593236fbad5facf43
dhiraj-patel <dhirajsuperhero@gmail.com>
2016-05-11 19:19:37 -0400

    working runningmedian

--- 14RunningMedian/Driver.java
+++ 14RunningMedian/Driver.java
@@ -1,22 +1,9 @@
 public class Driver{
     public static void main(String[] args){
-
-	RunningMedian m=new RunningMedian();
-	System.out.println(m.getMedian());
-	m.add(1);
-	System.out.println(m.getMedian());
-	m.add(2);
-	System.out.println(m.getMedian());
-	m.add(0);
-	System.out.println(m.getMedian());
-	m.add(3);
-	System.out.println(m.getMedian());
-	m.add(3);
-	m.add(3);
-	m.add(3);	
-	m.add(3);
-	System.out.println(m.getMedian());
-
-
+	RunningMedian m1 = new RunningMedian();
+	m1.add(2);
+	m1.add(5);
+	m1.add(3);
+	System.out.println(m1.getMedian());
     }
 }
--- 14RunningMedian/RunningMedian.java
+++ 14RunningMedian/RunningMedian.java
@@ -9,35 +9,56 @@ public class RunningMedian{
     }
 
     public double getMedian(){
-	if(max.size()==0&&min.size()==0){
+	if(max.size()==0 && min.size()==0){
 	    throw new NoSuchElementException();
 	}
-	else if (max.size() == 1 && min.size() == 0){
-	    return max.peek();
-	}
-	else if(max.size() == min.size() - 1 || max.size() == min.size() + 1){
+	else if (min.size()>max.size()){
 	    return min.peek();
+	}
+	else if(max.size()>min.size()){
+	    return max.peek();
 	}
 	else{
 	    return (min.peek() + max.peek()) / 2;
 	}
     }
+    /*
+    public void add(Integer x){
+	if(min.size()==0 && max.size() == 0){
+	    min.add(x);
+	}
+	else if(x<getMedian()){
+	    min.add(x);
+	    if(min.size()-max.size()>1){
+		max.add(min.remove());
+	    }
+	}
+	else{
+	    max.add(x);
+	    if(max.size()-min.size()>1){
+		min.add(max.remove());
+	    }
+	}
+    }
+    */
     public void add(Integer x){
 	try{
-	    if(x>getMedian()){
+	    if(x<getMedian()){
 		min.add(x);
 	    }else{
 		max.add(x);
 	    }
 	}
 	catch(NoSuchElementException e){
-	    max.add(x);
+	    min.add(x);
 	}
-	if (max.size() > min.size() + 1){
+	if(max.size()-min.size()>1){
 	    min.add(max.remove());
 	}
-	else if(min.size() > max.size() + 1){
+	if(min.size()-max.size()>1){
 	    max.add(min.remove());
 	}
+
     }
+	    
 }

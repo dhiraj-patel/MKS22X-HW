cacaa25dd4bb4f308621b46edc16311915773d90
dhiraj-patel <dhirajsuperhero@gmail.com>
2016-05-11 00:21:44 -0400

    trying runningmedian lol

--- 14RunningMedian/RunningMedian.java
+++ 14RunningMedian/RunningMedian.java
@@ -1,51 +1,43 @@
 import java.util.*;
 public class RunningMedian{
-    private MyHeap<Integer>min;
-    private MyHeap<Integer>max;
-    //Create an empty running median set.
+    private MyHeap<Integer> max;
+    private MyHeap<Integer> min;
+
     public RunningMedian(){
-	min = new MyHeap<Integer>(true);
-	max = new MyHeap<Integer>(false);
+	max = new MyHeap<Integer>(true);
+	min = new MyHeap<Integer>(false);
     }
-    //  When empty: throws new NoSuchElementException()
-    //  Returns the median value
+
     public double getMedian(){
-	if(min.size() == 0 || max.size() == 0){
+	if(max.size()==0&&min.size()==0){
 	    throw new NoSuchElementException();
 	}
-	if(min.size()>max.size()){
-	    return min.peek();
-	}
-	else if(min.size()<max.size()){
+	else if (max.size() == 1 && min.size() == 0){
 	    return max.peek();
 	}
+	else if(max.size() == min.size() - 1 || max.size() == min.size() + 1){
+	    return min.peek();
+	}
 	else{
-	    return((min.peek()+max.peek())/2);
+	    return (min.peek() + max.peek()) / 2;
 	}
     }
-    //  add to the "SmallValue" heap if  x < median, 
-    //  add to the "BigValue" heap otherwise. 
-    //  balance the two heaps so that their size differ by no more than 1. 
     public void add(Integer x){
-	if(min.size()+max.size()==0){
-	    min.add(x);
+	try{
+	    if(x>getMedian()){
+		min.add(x);
+	    }else{
+		max.add(x);
+	    }
 	}
-	else if(x<getMedian()){
-	    min.add(x);
-	    if(min.size()-max.size()>1){
-		max.add(min.remove());
-	    }
-	}else{
+	catch(NoSuchElementException e){
 	    max.add(x);
-	    if(max.size()-min.size()>1){
-		min.add(max.remove());
-	    }
+	}
+	if (max.size() > min.size() + 1){
+	    min.add(max.remove());
+	}
+	else if(min.size() > max.size() + 1){
+	    max.add(min.remove());
 	}
     }
-	    
-    
-
-
-
-
 }
